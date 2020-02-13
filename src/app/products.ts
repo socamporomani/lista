0617c8fbaf7d9688d1171af7b92d854b661cb7ba
socamporomani <socamporomani@danielcastelao.org>
2020-02-13 13:12:57 +0100

    1ºer comit que soluciona los problemas iniciales

---
+++ src/app/products.ts
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    name: 'Manzanas',
+    price: 1,
+    moneda:'€/Kg',
+    description: 'Manzana Golden'
+  },
+  {
+    name: 'Plátanos',
+    price: 1,
+    moneda:'€/Kg',
+    description: 'Los Plátanos de canarias'
+  },
+  {
+    name: 'Piña',
+    price: 3,
+    moneda:'€/kg',
+    description: 'No es para pizza'
+  }
+];
